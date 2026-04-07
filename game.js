60c241640d0e20a9d8ca5f8b803da37bb4336dfc
leoraggy <leoragual35@gmail.com>
2026-04-07 11:05:45 -0700

    Rendered upgrades

--- game.js
+++ game.js
@@ -2,25 +2,25 @@ let score = 0;
 let pointsPerClick = 1;
 
 let upgrades = [
-    {
-        name: "Plus 1",
-        id: 1,
-        cost: 5,
-        bonus: 3
-    },
-    {
-        name: "Plus 2",
-        id: 2,
-        cost: 7,
-        bonus: 4
-    },
-    {
-        name: "Plus 3",
-        id: 3,
-        cost: 9,
-        bonus: 5
-    },
-]
+  {
+    name: "Plus 1",
+    id: 1,
+    cost: 5,
+    bonus: 3,
+  },
+  {
+    name: "Plus 2",
+    id: 2,
+    cost: 7,
+    bonus: 4,
+  },
+  {
+    name: "Plus 3",
+    id: 3,
+    cost: 9,
+    bonus: 5,
+  },
+];
 
 const updateDisplay = () => {
   document.getElementById("score-display").textContent = "Score: " + score;
@@ -33,3 +33,19 @@ document.getElementById("click-btn").addEventListener("click", () => {
   updateDisplay();
 });
 
+const renderUpgrades = () => {
+  upgrades.forEach((upgrade) => {
+    const upgradeDiv = document.createElement("div");
+
+    upgradeDiv.setAttribute("id", upgrade.id);
+
+    const name = document.createElement("p");
+    name.textContent = `${upgrade.name} Cost: ${upgrade.cost} Bonus: ${upgrade.bonus}`;
+
+    upgradeDiv.appendChild(name);
+
+    document.getElementById("upgrades").appendChild(upgradeDiv);
+  });
+};
+
+renderUpgrades();
