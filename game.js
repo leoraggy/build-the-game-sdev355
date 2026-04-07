2d5ee088653fe9c38041fdda371e2017f748ea00
leoraggy <leoragual35@gmail.com>
2026-04-07 10:49:16 -0700

    Completed step 2 track the score

---
+++ game.js
@@ -0,0 +1,13 @@
+let score = 0;
+let pointsPerClick = 1;
+
+const updateDisplay = () => {
+  document.getElementById("score-display").textContent = "Score: " + score;
+  document.getElementById("rate-display").textContent =
+    "Points per click: " + pointsPerClick;
+};
+
+document.getElementById("click-btn").addEventListener("click", () => {
+  score += pointsPerClick;
+  updateDisplay();
+});
